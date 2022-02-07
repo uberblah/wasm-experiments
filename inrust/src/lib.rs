7a98b16d554e0d276683b4554d5015fa0d6441d4
uberblah <miles.rufatlatre@gmail.com>
2022-02-06 16:21:17 -0800

    wasm by hand and in rust

---
+++ inrust/src/lib.rs
@@ -0,0 +1,48 @@
+/**
+ * This is how we declare imported methods in Rust's WASM unknown target.
+ * 
+ * Note that imported and exported methods must only accept and return values
+ * which can be hidden behind an i32 representation.
+ */
+extern "C" {
+    fn notifyDone(value: i32);
+}
+
+/**
+ * We can no longer use the zero address as the start of our IO buffer,
+ * because Rust needs that space for data. We will use this static buffer
+ * as the IO buffer.
+ */
+#[no_mangle]
+pub static mut XCHG_BUF: [u8; 2] = [0; 2];
+
+/**
+ * We need a way to tell the runtime where our static buffer is.
+ */
+#[no_mangle]
+pub unsafe fn get_xchg() -> i32 {
+    XCHG_BUF.as_mut_ptr() as i32
+}
+
+/**
+ * Add the two numbers, let the caller know about it,
+ * and then grab the value they sent us in the IO buffer.
+ * 
+ * In a real application we might pass encoded data structures
+ * in the IO buffer, both as input and output to our functions.
+ */
+#[no_mangle]
+pub unsafe fn add_2_nums(a: i32, b: i32) -> i32 {
+    notifyDone(a + b); // call a callback provided by the caller, using args they provided
+    XCHG_BUF[1] = 0xff; // write a hardcoded value to memory for the caller to find
+    (XCHG_BUF[0] as i32) + a + b // return a value to the caller based on args and a value they wrote to memory
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
